bc57012212d3f641a40250c149fdce583f63a43a
Forrest Ye <afu@forresty.com>
2013-08-10 15:24:52 +0800

    moving plane

--- FortyTwo/FTTGameViewController.m
+++ FortyTwo/FTTGameViewController.m
@@ -13,6 +13,12 @@
 
 @interface FTTGameViewController ()
 
+@property (nonatomic) CMMotionManager *motionMannager;
+
+@property (nonatomic) NSOperationQueue *motionHandlingQueue;
+
+@property (nonatomic) UIView *planeView;
+
 @end
 
 
@@ -36,6 +42,11 @@
 
   if (self) {
     self.wantsFullScreenLayout = YES;
+
+    self.motionHandlingQueue = [[NSOperationQueue alloc] init];
+
+    self.motionMannager = [[CMMotionManager alloc] init];
+    self.motionMannager.accelerometerUpdateInterval = 0.1;
   }
 
   return self;
@@ -45,7 +56,29 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  self.view.backgroundColor = [UIColor yellowColor];
+  self.view.backgroundColor = [UIColor blackColor];
+
+  self.planeView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 10, 10)];
+  self.planeView.backgroundColor = [UIColor whiteColor];
+
+  [self.view addSubview:self.planeView];
+
+  __weak FTTGameViewController *weakSelf = self;
+  [self.motionMannager startAccelerometerUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+
+//    NSLog(@"acc %@", accelerometerData);
+    CGFloat newX = weakSelf.planeView.center.x + accelerometerData.acceleration.x * 10;
+    CGFloat newY = weakSelf.planeView.center.y - accelerometerData.acceleration.y * 10;
+
+    newX = MAX(0, newX);
+    newX = MIN(320, newX);
+    newY = MAX(0, newY);
+    newY = MIN(480, newY);
+
+    [UIView animateWithDuration:0.09 animations:^{
+      weakSelf.planeView.center = CGPointMake(newX, newY);
+    }];
+  }];
 }
 
 
